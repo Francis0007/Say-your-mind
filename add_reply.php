15edd20a766c1d6d70261cc5c0fd7201481ea57a
Francis0007 <oshifrancis2@gmail.com>
2024-12-26 11:25:59 -0800

    first commit

---
+++ add_reply.php
@@ -0,0 +1,20 @@
+<?php
+$conn = new mysqli("localhost", "root", "", "mind_say");
+if ($conn->connect_error) die("Connection failed: " . $conn->connect_error);
+
+$post_id = $_POST['post_id'];
+$user_name = $_POST['user_name'];
+$reply = $_POST['reply'];
+
+$stmt = $conn->prepare("INSERT INTO replies (post_id, user_name, reply) VALUES (?, ?, ?)");
+$stmt->bind_param("iss", $post_id, $user_name, $reply);
+
+if ($stmt->execute()) {
+    header("Location: index.php");
+} else {
+    echo "Error: " . $stmt->error;
+}
+
+$stmt->close();
+$conn->close();
+?>
